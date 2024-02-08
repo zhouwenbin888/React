a7fcb028483f4051f8e88f5e06fb3bba62fb3edc
zhouwenbin888 <1146872611@qq.com>
2024-02-08 19:47:34 +0800

    update

--- jikeyuan/craco.config.js
+++ jikeyuan/craco.config.js
@@ -1,9 +1,38 @@
 const path = require('path');
+const { whenProd } = require('@craco/craco');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
     webpack: {
         alias: {
             '@': path.resolve(__dirname, 'src')
         },
+        configure: (webpackConfig) => {
+            let cdn = {
+                js: []
+            };
+            whenProd(() => {
+                webpackConfig.externals = {
+                    react: 'React',
+                    'react-dom': 'ReactDOM'
+                };
+                cdn = {
+                    js: [
+                        'https://cdnjs.cloudflare.com/ajax/libs/react/18.1.0/umd/react.production.min.js',
+                        'https://cdnjs.cloudflare.com/ajax/libs/react-dom/18.1.0/umd/react-dom.production.min.js',
+                    ]
+                };
+            });
+
+            const htmlWebpackPlugin = webpackConfig.plugins.find(
+                (plugin) => plugin instanceof HtmlWebpackPlugin
+            );
+
+            if (htmlWebpackPlugin) {
+                htmlWebpackPlugin.options.cdn = cdn;
+            }
+
+            return webpackConfig;
+        }
     }
 };
